f5581eaf5b0f658247503654c2d4c8a1b35b176a
tech <asifneyaz@gmail.com>
2021-07-27 22:20:16 -0500

    test

--- ratings-counter.py
+++ ratings-counter.py
@@ -14,10 +14,10 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile("file:////home/datalakedata/mysparkprojects/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
-print(ratings)
+print(type(ratings))
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
-    
+
