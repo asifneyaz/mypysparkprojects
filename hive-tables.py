72ba372356b7e81d3a8ec898e3341d2080e684da
tech <asifneyaz@gmail.com>
2021-07-28 07:29:13 -0500

    new code

--- hive-tables.py
+++ hive-tables.py
@@ -15,7 +15,7 @@ spark = SparkSession \
 
 # spark is an existing SparkSession
 spark.sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) USING hive")
-spark.sql("LOAD DATA LOCAL INPATH 'examples/src/main/resources/kv1.txt' INTO TABLE src")
+spark.sql("LOAD DATA LOCAL INPATH '/home/datalakedata/mysparkprojects/kv1.txt' INTO TABLE src")
 
 # Queries are expressed in HiveQL
 spark.sql("SELECT * FROM src").show()
