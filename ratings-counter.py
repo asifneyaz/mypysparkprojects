3da5d681d0dc847b6da8ba8a3a2c023804d063f0
tech <asifneyaz@gmail.com>
2021-07-27 19:55:17 -0500

    made changes Please enter the commit message for your changes. Lines starting

--- ratings-counter.py
+++ ratings-counter.py
@@ -10,9 +10,9 @@ from pyspark import SparkConf, SparkContext
 import collections
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
-lines = sc.textFile("file:///usr/techbox/codes/mysparkprojects/ml-100k/u.data")
+lines = sc.textFile("file:////home/datalakedata/mysparkprojects/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
