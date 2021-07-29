8ef251d321b10ae565ce5c0fcaa4c556988da8ac
tech <asifneyaz@gmail.com>
2021-07-29 09:54:45 -0500

    new code

--- ratings-counter.py
+++ ratings-counter.py
@@ -8,6 +8,7 @@
 #my first python code
 from pyspark import SparkConf, SparkContext
 import collections
+from pyspark.sql import SparkSession
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf=conf)
@@ -19,4 +20,6 @@ result = ratings.countByValue()
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
+
+print('hello')
 
